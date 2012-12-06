b5cdc47bc28ccbba694bc5f2663576ace19dd159
Sam Phippen <samphippen@googlemail.com>
2012-12-06 21:13:28 +0000

    Add yolo web service
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, url_for
+import json
+import os
+import urllib2
+from random import choice
+app = Flask(__name__)
+
+url = "http://search.twitter.com/search.json?q=yolo&rpp=50&include_entities=true&with_twitter_user_id=true&result_type=mixed"
+
+url2 = "http://api.semetric.com/sentiment?token=2a4277ac1ae94261aabce70284506e1f&text="
+
+@app.route("/")
+def index():
+    return open("templates/index.html").read()
+
+
+@app.route("/yolo")
+def yolo():
+    tweets = json.loads(urllib2.urlopen(url).read())
+    result = choice(tweets["results"])
+    text = result["text"]
+    author = result["from_user"]
+    madness_json = urllib2.urlopen(url2+urllib2.quote(text.encode("utf-8"))).read()
+    p = json.loads(madness_json)
+    madness = 5-p["response"][0]["score"]
+
+    return text + "<br><br><b>" + author + "</b> Madness Index: " + str(madness)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
